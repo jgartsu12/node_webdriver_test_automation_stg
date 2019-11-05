d30e742c0665603e83427449fb85ab4f122e7081
jgartsu12 <jgartsu12@gmail.com>
2019-11-04 21:04:43 -0500

    js is async, added timeout and driver var

--- node_webdriver/challenge1.js
+++ node_webdriver/challenge1.js
@@ -3,6 +3,8 @@ var webdriver = require('selenium-webdriver');
 var assert = require("chai").assert;
 
 describe("challenge1 suite", function(){
+    this.timeout(20000);
+    var driver;
     before (function() {
     });
 
