4946103e5af7efb3fd855da95a42953266bd7fb7
jgartsu12 <jgartsu12@gmail.com>
2019-11-04 21:02:25 -0500

    imported chrome driver and created assert

--- node_webdriver/challenge1.js
+++ node_webdriver/challenge1.js
@@ -1,3 +1,7 @@
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var assert = require("chai").assert;
+
 describe("challenge1 suite", function(){
     before (function() {
     });
