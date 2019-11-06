55576ae8d226c560d06745206fbb69158c45fdce
jgartsu12 <jgartsu12@gmail.com>
2019-11-05 19:35:02 -0500

    added search for exotics and porsche

--- node_js_challenges/challenge2_assert.js
+++ node_js_challenges/challenge2_assert.js
@@ -1,1 +1,39 @@
 // use hard assertion 
+require('chromedriver');
+var webdriver = require('selenium-webdriver');
+var assert = require("chai").assert;
+
+describe("challenge2 suite", function(){
+   this.timeout(20000);
+   var driver;
+   before(function () {
+       // initializing chrome driver
+       driver = new webdriver.Builder()
+       .withCapabilities(webdriver.Capabilities.chrome())
+       .build();
+   });
+
+
+
+   after(function () {
+       return driver.quit();
+   });
+
+   it("I open the copart website", function() {
+       return driver.get("http://www.copart.com");
+   });
+
+   it("These cars are 'exotics'", function() {
+       return driver.getTypes().then(function(Types) {
+           assert.equal(Types, "exotics");
+       });
+   });
+
+   it("This verifies 'Porsche'", function() {
+       return driver.getMakes().then(function(Makes) {
+            assert.equal(Makes, "Porsche")
+       });
+   });
+
+
+});
