f4a438ce90f12a587a75c99650d8401480a04181
jgartsu12 <jgartsu12@gmail.com>
2019-11-07 10:31:54 -0500

    es6

--- node_js_challenges/challenge1.js
+++ node_js_challenges/challenge1.js
@@ -1,14 +1,13 @@
-require('chromedriver');
-var webdriver = require('selenium-webdriver');
-var assert = require("chai").assert;
+import 'chromedriver';
+import { Builder, Capabilities } from 'selenium-webdriver';
+import { assert } from "chai";
 
 describe("challenge1 suite", function(){
    this.timeout(20000);
    var driver;
    before(function () {
-       // initializing chrome driver
-       driver = new webdriver.Builder()
-       .withCapabilities(webdriver.Capabilities.chrome())
+       driver = new Builder()
+       .withCapabilities(Capabilities.chrome())
        .build();
    });
 
@@ -23,7 +22,6 @@ describe("challenge1 suite", function(){
    });
 
    it("The title is 'Google'", function() {
-       // Since we want the title from the page, we need to manually handle the Promise
        return driver.getTitle().then(function(title) {
            assert.equal(title, "Google");
        });
